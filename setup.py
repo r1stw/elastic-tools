b4de4922f7138d2e1edee09ffd6664961dc3af4e
a.pleskanovsky <a.pleskanovsky@corp.nekki.ru>
2017-03-02 17:16:44 +0300

    now it's a package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='elastic-tools',
+      version='0.0.0',
+      description='Elastic search requests wrapper',
+      url='https://github.com/pleskanovsky/elastic-tools',
+      author='pleskanovsky',
+      author_email='vnezapno.pochta@gmail.com',
+      license='MIT',
+      packages=['elastic-tools'],
+      zip_safe=False)
