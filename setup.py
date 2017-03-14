08024057cc7406e4b841c97669456907721fe02e
a.pleskanovsky <a.pleskanovsky@corp.nekki.ru>
2017-03-14 17:48:30 +0300

    version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='elastictools',
-      version='0.0.0',
+      version='0.0.1',
       description='Elastic search requests wrapper',
       url='https://github.com/pleskanovsky/elastictools',
       author='pleskanovsky',
