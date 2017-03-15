22a933c1f255fc3e50b2b34a1e3dc4e518c7f7ed
pleskanovsky <vnezapno.pochta@gmail.com>
2017-03-15 18:29:10 +0300

    another typo in default connection

--- elastictools/elasticclient/elasticclient.py
+++ elastictools/elasticclient/elasticclient.py
@@ -53,7 +53,7 @@ def get_credentials(path_to_credentials_file):
         connections[x.name] = x
 
     if "default" not in connections:
-        connections["default"] = next(iter(connections))
+        connections["default"] = connections[next(iter(connections))]
     else:
         print("Warning: pre-defined \"default\" credentials found. Reassinging default in runtime will override them")
 
