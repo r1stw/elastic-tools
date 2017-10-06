6b89e956648389c619fcf4a191dd62e03af3d215
skyhound <vnezapno.pochta@gmail.com>
2017-10-06 19:37:50 +0300

    init fix

--- elastictools/elasticclient/__init__.py
+++ elastictools/elasticclient/__init__.py
@@ -2,3 +2,4 @@ from .elasticclient import get_credentials
 from .elasticclient import assign_default
 from .elasticclient import search
 from .elasticclient import Request
+from .elasticclient import ElasticToolsTransportError
